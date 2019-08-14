9e5ead7487175963929f5492bf7865de4ab6f97e
Jack Bridger <jacksbridger@gmail.com>
2019-08-14 16:57:22 +0100

    working on the client side validation

---
+++ public/validation.js
@@ -0,0 +1,22 @@
+console.log('hello');
+const newUsernameField = document.querySelector('#new-username');
+const loginUsernameField = document.querySelector('#login-username');
+const newPasswordField = document.querySelector('#new-password');
+const loginPasswordField = document.querySelector('#login-password');
+const createForm = document.querySelector('.create-form');
+const newUserForm = document.querySelector('.login-form');
+// submit event listener on the form that checks validity using validity.patternmismatch 
+// and sets a custom validity and also prevents default
+
+createForm.addEventListener('submit', (event) => {
+    if (newPasswordField.validity.valueMissing) {
+        newPasswordField.setCustomValidity('Please enter a password')
+        event.preventDefault();
+    }
+})
+
+// newUsernameField.setCustomValidity('Username must be lowercase letters and numbers only');
+// loginUsernameField.setCustomValidity('Username must be lowercase letters and numbers only');
+
+// newPasswordField.setCustomValidity('Password must be 6 characters with at least one lower case, one upper case and one number');
+// loginPasswordField.setCustomValidity('Password must be 6 characters with at least one lower case, one upper case and one number');
